cb062a8ce14cbb6051d427b2fde9ff159ac55e5d
apurvjoy <apurvjoy7@gmail.com>
2025-10-15 22:17:45 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,30 @@
+/*const age = 24;
+console.log(age);
+
+let a = 23;
+
+let fullName="Surbhi";
+console.log(fullName);*/
+
+/*let Student ={
+    fullName:"Apurv",
+    age:23,
+    cgpa:8.84,
+    isPass:true,
+};
+
+Student["age"]= Student["age"]+1;
+
+console.log(Student["age"]);*/
+
+const product={
+    productName:"Parker Pen",
+    rating:4,
+    offer:5,
+    price:270,
+    isOffer:true,
+};
+
+console.log(typeof product["rating"]);
+
+
---
+++ script.js
@@ -0,0 +1,22 @@
+//Arithmetic Operators
+
+// let a= 5;
+// let b= 4;
+
+// console.log("a + b =",a+b);
+// console.log("a - b =",a-b);
+// console.log("a * b =",a*b);
+// console.log("a / b =",a/b);
+// console.log("a % b =",a%b);
+// console.log("a ** b =",a**b);
+
+//Unary Operator 
+
+let a=6;
+let b=8;
+
+console.log("a =",a, "& b =",b);
+a++;
+console.log("a =",a);
+b--;
+console.log("b =",b);
