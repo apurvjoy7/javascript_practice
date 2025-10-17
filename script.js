a5245be4f1b1ca17f694e7367916fcc482f95bb9
apurvjoy7 <apurvjoy7@gmail.com>
2025-10-17 16:25:06 +0530

    Conditional Statements

--- script.js
+++ script.js
@@ -47,20 +47,129 @@
 
 //Logical Opertor
 
-let a=6;
-let b=5;
+// let a=6;
+// let b=5;
 
-let cond1= a>b;
-let cond2= a===b;
+// let cond1= a>b;
+// let cond2= a===b;
 
-console.log("cond1 && cond2 =",cond1 && cond2);
+// console.log("cond1 && cond2 =",cond1 && cond2);
 
-let cond3= a>b;
-let cond4= a===6;
+// let cond3= a>b;
+// let cond4= a===6;
 
-console.log("cond3 && cond4 =",cond3 && cond4);
+// console.log("cond3 && cond4 =",cond3 && cond4);
 
-console.log("cond1 || cond2 =",cond1 || cond2);
-console.log("!cond1 =",!(a>b));
+// console.log("cond1 || cond2 =",cond1 || cond2);
+// console.log("!cond1 =",!(a>b));
 
+// Conditional Statements
+
+//(i) If Statement
+
+// let mode= "dark";
+// let color;
+
+// if(mode==="dark")
+// {
+//     color="black";
+// }
+// if(mode==="light")
+// {
+//     color="white";
+// }
+// console.log(color);
+
+// let age=18;
+// if(age>=18)
+// {
+//     console.log("You can Vote");
+// }
+// if(age<18)
+// {
+//     console.log("You cannot vaote");
+// }
+
+//(ii) If Else Statement
+
+// let age=25;
+// if(age>=18)
+// {
+//     console.log("Can Vote");
+// }
+// else
+// {
+//     console.log("Cannot Vote");
+// }
+
+//(iii) Else-If Statement
+
+// let mode="dark";
+// let color;
+
+// if(mode==="dark")
+// {
+//     color="black";
+// }
+// else if(mode==="blue")
+// {
+//     color="blue";
+// }
+// else if(mode==="pink")
+// {
+//     color="pink";
+// }
+// else
+// {
+//     color="white";
+// }
+
+// console.log(color);
+
+//Ternary Operators
+
+// let age=24;
+
+// let result= age>=18 ? "adult":"not adult";
+// console.log(result);
+
+// Practice Questions
+
+//(i) num is multiple of 5 or not.
+
+// let num= prompt("Enter a Number:");
+
+// if(num % 5===0)
+// {
+//     console.log(num,"is a multiple of 5");
+// }
+// else
+// {
+//     console.log(num,"is not a multiple of 5");
+// }
+
+//(ii) Scores Grading
+
+// let score=91;
+
+// if(score>=90 && score<=100)
+// {
+//     console.log("Grade A");
+// }
+// else if(score>=70 && score<=89)
+// {
+//     console.log("Grade B");
+// }
+// else if(score>=60 && score<=69)
+// {
+//     console.log("Grade C");
+// }
+// else if(score>=50 && score<=59)
+// {
+//     console.log("Grade D");
+// }
+// if(score>=0 && score<=49)
+// {
+//     console.log("Grade F");
+// }
 
