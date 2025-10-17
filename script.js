3377a78a81e420c3ff75172c4325d28a04e4f4d8
apurvjoy7 <apurvjoy7@gmail.com>
2025-10-17 10:45:46 +0530

    types of opertor

--- script.js
+++ script.js
@@ -12,11 +12,31 @@
 
 //Unary Operator 
 
-let a=6;
-let b=8;
+// let a=6;
+// let b=8;
 
-console.log("a =",a, "& b =",b);
-a++;
-console.log("a =",a);
-b--;
-console.log("b is =",b);
+// console.log("a =",a, "& b =",b);
+// a++;
+// console.log("a =",a);
+// b--;
+// console.log("b is =",b);
+
+// Assignment Operator
+// let a=10;
+// let b=5;
+
+// a += 4;
+
+// console.log("a =",a);
+// b-=2;
+// console.log("b =",b);
+
+//Comparison Operator
+
+let a=2;
+let b="2";
+
+// console.log("a == b",a == b);
+// console.log("a != b",a != b);
+console.log("a === b",a === b);
+console.log("a !== b",a !== b);
