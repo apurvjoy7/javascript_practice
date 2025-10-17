e42cf873c3c6a7b19f61dbb780940736cb79d023
apurvjoy7 <apurvjoy7@gmail.com>
2025-10-17 11:18:11 +0530

    update

--- script.js
+++ script.js
@@ -33,10 +33,34 @@
 
 //Comparison Operator
 
-let a=2;
-let b="2";
+// let a=2;
+// let b=4;
 
 // console.log("a == b",a == b);
 // console.log("a != b",a != b);
-console.log("a === b",a === b);
-console.log("a !== b",a !== b);
+// console.log("a === b",a === b);
+// console.log("a !== b",a !== b);
+// console.log("a > b",a > b);
+// console.log("a < b",a < b);
+// console.log("a >= b",a >= b);
+// console.log("a <= b",a <= b);
+
+//Logical Opertor
+
+let a=6;
+let b=5;
+
+let cond1= a>b;
+let cond2= a===b;
+
+console.log("cond1 && cond2 =",cond1 && cond2);
+
+let cond3= a>b;
+let cond4= a===6;
+
+console.log("cond3 && cond4 =",cond3 && cond4);
+
+console.log("cond1 || cond2 =",cond1 || cond2);
+console.log("!cond1 =",!(a>b));
+
+
