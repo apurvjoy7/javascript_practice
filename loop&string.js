38128db5a15f18d46c93f02f61478ae2c660bac5
apurvjoy7 <apurvjoy7@gmail.com>
2025-10-18 12:31:20 +0530

    loop

---
+++ loop&string.js
@@ -0,0 +1,76 @@
+//Loop
+//(i) For Loop
+
+// for(let i=1;i<=5;i++)
+// {
+//     console.log("Apurv");
+// }
+
+// let sum=0;
+// for(let i=1;i<=5;i++)
+// {
+//     sum=sum+i;
+// }
+// console.log("Sum is",sum);
+// console.log("Loop has ended");
+
+//(ii) While Loop
+
+// let i=1;
+// while(i<=5)
+// {
+//     console.log("Anamika");
+//     i++;
+// }
+
+//(iii) Do-While Loop
+
+// let i=1;
+// do{
+//     console.log("Joy");
+//     i++;
+// }while(i<=5);
+
+//(iv) for-of Loop
+
+// let str="Anamika";
+// for(let i of str)
+// {
+//     console.log("i =",i);
+// }
+
+// let str="Apurv";
+// let size=0;
+// for(let i of str)
+// {
+//     console.log("i =",i);
+//     size++;
+// }
+// console.log("Size is",size);
+
+//(v) for-in Loop
+
+// let student={
+//     name:"Apurv",
+//     age:23,
+//     cgpa:8.84,
+//     isPass:true,
+// };
+
+// for(let key in student)
+// {
+//     console.log("Key =",key,"Value =",student[key]);
+// }
+
+//Practice Questions
+
+// (i)
+
+// for(let num=0;num<=100;num++)
+// {
+//     if(num%2===0)
+//     {
+//         console.log("Num =",num);
+//     }
+// }
+
